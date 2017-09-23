c5e85122f5b56a9e95ac5c0d581d6d11ec9048c3
r21nomi <ryotakosu@gmail.com>
2017-09-23 17:35:11 +0900

    Use template

--- application.go
+++ application.go
@@ -4,7 +4,14 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"text/template"
 )
+
+type Art struct {
+	ID string
+    Title string
+    Body int
+}
 
 func main() {
 	port := os.Getenv("PORT")
@@ -12,17 +19,32 @@ func main() {
 		port = "5000"
 	}
 
-	const indexPage = "public/arto-web/html/index.html"
-	const detailPage = "public/arto-web/html/detail.html"
+	// Pages
+	const indexPage = "public/arto-web/template/page/index.html"
+	const detailPage = "public/arto-web/template/page/detail.html"
+
+	// Partials
+	const headerPartial = "public/arto-web/template/partial/header.html"
+	const artItemPartial = "public/arto-web/template/partial/art_item.html"
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		log.Printf("Serving %s to %s...\n", indexPage, r.RemoteAddr)
-		http.ServeFile(w, r, indexPage)
+		data := map[string]interface{}{
+			"Items": []Art{
+				Art{ID: "1", Title: "Art1", Body: 300},
+				Art{ID: "2", Title: "Art2", Body: 500},
+				Art{ID: "3", Title: "Art3", Body: 600},
+				Art{ID: "4", Title: "Art4", Body: 300},
+			},
+		}
+
+		t, _ := template.ParseFiles(indexPage, headerPartial, artItemPartial)
+		t.ExecuteTemplate(w, "index", data)
 	})
 
+	// TODO: Handle "/detail/{ID}"
 	http.HandleFunc("/detail", func(w http.ResponseWriter, r *http.Request) {
-		log.Printf("Serving %s to %s...\n", indexPage, r.RemoteAddr)
-		http.ServeFile(w, r, detailPage)
+		t, _ := template.ParseFiles(detailPage, headerPartial)
+		t.ExecuteTemplate(w, "detail", "")
 	})
 
 	log.Printf("Listening on port %s\n\n", port)
