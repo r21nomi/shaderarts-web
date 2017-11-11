88108366d30501934114f45756919cda5b2e55e2
r21nomi <ryotakosu@gmail.com>
2017-11-11 19:28:53 +0900

    Use CSS-in-JS anyway

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,8 +1,8 @@
 import React from 'react'
-import styles from '../styles/header.css'
+import styles from '../styles/header.css.js'
 
 const Header = () => (
-    <header className={styles.header}>
+    <header style={styles.header}>
         Arto
     </header>
 )
