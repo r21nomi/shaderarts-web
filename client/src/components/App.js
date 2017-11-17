36a7a7c0c159b6d635dec3892f5226579302d263
r21nomi <ryotakosu@gmail.com>
2017-11-17 16:33:51 +0800

    Make Filter component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import Header from './Header'
+import Filter from './Filter'
 import Footer from './Footer'
 import AddTodo from '../containers/AddTodo'
 import VisibleTodoList from '../containers/VisibleTodoList'
 import 'normalize.css'
+import styles from '../styles/app.css'
 
 const App = () => (
   <div>
     <Header />
-    <AddTodo />
-    <VisibleTodoList />
+    <div className={styles.content}>
+      <AddTodo />
+      <Filter />
+      <VisibleTodoList />
+    </div>
     <Footer />
   </div>
 )
--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,21 +1,10 @@
 import React from 'react'
-import FilterLink from '../containers/FilterLink'
 import styles from '../styles/footer.css'
 
 const Footer = () => (
     <footer className={styles.footer}>
         <p>
-            Show:
-            {" "}
-            <FilterLink filter="SHOW_ALL">
-                ALL
-            </FilterLink>
-            <FilterLink filter="SHOW_COMPLETED">
-                Completed
-            </FilterLink>
-            <FilterLink filter="SHOW_ACTIVE">
-                Active
-            </FilterLink>
+            Arto, inc.
         </p>
     </footer>
 )
