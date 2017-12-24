f4acaf59974094da1fd74d5cda533dd765cc379d
r21nomi <ryotakosu@gmail.com>
2017-12-24 18:34:08 +0900

    Refactor

--- client/src/actions/loginTwitter.tsx
+++ client/src/actions/loginTwitter.tsx
@@ -11,25 +11,9 @@ export function loginTwitter() {
     return (dispatch: any) => {
         var provider = new firebase.auth.TwitterAuthProvider();
         firebase.auth().signInWithPopup(provider).then(function(result) {
-            // var token = result.credential.accessToken;
-            // var secret = result.credential.secret;
             var user = result.user;
             dispatch(loginSuccess(user));
 
-            // firebase.auth().onAuthStateChanged in fetchUser.tsx will be fired.
-
-            // var credential = firebase.auth.TwitterAuthProvider.credential(token, secret);
-            // firebase.auth().signInWithCredential(credential)
-            //     .catch(function(error) {
-            //         var errorMessage = error.message;
-            //         // // The email of the user's account used.
-            //         // var email = error.email;
-            //         // // The firebase.auth.AuthCredential type that was used.
-            //         // var credential = error.credential;
-            //         console.log(errorMessage);
-            //     }).then(function(y) {
-            //         console.log(y);
-            //     });
           }).catch(function(error) {
             // Handle Errors here.
             var errorCode = error.code;
