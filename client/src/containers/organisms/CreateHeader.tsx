2f4a6c8ef375ad8bd81ad06aeb147dcaef1699c5
r21nomi <ryotakosu@gmail.com>
2018-01-08 03:08:08 +0900

    Add event handler for submit button

--- client/src/containers/organisms/CreateHeader.tsx
+++ client/src/containers/organisms/CreateHeader.tsx
@@ -3,16 +3,39 @@ import { Link } from 'react-router-dom';
 import './styles/header.css';
 import './styles/create_header.css';
 
-const CreateHeader = () => (
-    <header className="Header CreateHeader">
-        <div className="Header-content CreateHeader-content">
-            <div className="Header-logo CreateHeader-logo"><Link to="/">Arto</Link></div>
-            <ul className="Header-menu CreateHeader-menu">
-                <li className="Header-menuItem CreateHeader-menuItem"><a href="javascript:void(0)">Save as draft</a></li>
-                <li className="Header-menuItem CreateHeader-menuItem"><a href="javascript:void(0)">Submit</a></li>
-            </ul>
-        </div>
-    </header>
-);
+interface Props {
+    onSaveAsDraftButtonClick: () => void;
+    onSubmitButtonClick: () => void;
+}
+
+class CreateHeader extends React.Component<Props, object> {
+    render() {
+        const { onSaveAsDraftButtonClick, onSubmitButtonClick } = this.props;
+
+        return <header className="Header CreateHeader">
+            <div className="Header-content CreateHeader-content">
+                <div className="Header-logo CreateHeader-logo"><Link to="/">Arto</Link></div>
+                <ul className="Header-menu CreateHeader-menu">
+                    <li className="Header-menuItem CreateHeader-menuItem">
+                        <a
+                            href="javascript:void(0)"
+                            onClick={() => onSaveAsDraftButtonClick()}
+                        >
+                            Save as draft
+                        </a>
+                    </li>
+                    <li className="Header-menuItem CreateHeader-menuItem">
+                        <a
+                            href="javascript:void(0)"
+                            onClick={() => onSubmitButtonClick()}
+                        >
+                            Submit
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </header>;
+    }
+}
 
 export default CreateHeader;
--- client/src/containers/pages/CreatePage.tsx
+++ client/src/containers/pages/CreatePage.tsx
@@ -1,4 +1,7 @@
 import * as React from 'react';
+import { connect } from 'react-redux';
+import { RootState } from '../../reducers/index';
+import { CodeState } from '../../reducers/code';
 import UpdateCode from '../UpdateCode';
 import UpdateGLSLCanvas from '../UpdateGLSLCanvas';
 import CreateHeader from '../organisms/CreateHeader';
@@ -13,14 +16,45 @@ const canvasProps = {
     }
 };
 
-const CreatePage = () => (
-    <div>
-        <CreateHeader />
-        <div className="Page-content CreatePage-content">
-            <UpdateCode />
-            <UpdateGLSLCanvas {...canvasProps} />
-        </div>
-    </div>
-);
+interface Props {
+    code: CodeState;
+    handleHeaderSaveAsDraftButtonClick: (code: CodeState) => void;
+    handleHeaderSubmitButtonClick: (code: CodeState) => void;
+}
 
-export default CreatePage;
+const mapStateToProps = (state: RootState) => ({
+    code: state.code
+});
+
+const mapDispatchToProps = (dispatch: any, ownProps: any) => ({
+    handleHeaderSaveAsDraftButtonClick: (code: CodeState) => {
+        console.log("handleHeaderSaveAsDraftButtonClick");
+        console.log(code);
+    },
+    handleHeaderSubmitButtonClick: (code: CodeState) => {
+        console.log("handleHeaderSubmitButtonClick");
+        console.log(code);
+    }
+});
+
+class CreatePage extends React.Component<Props, object> {
+    render() {
+        const { code, handleHeaderSaveAsDraftButtonClick, handleHeaderSubmitButtonClick } = this.props
+
+        return <div>
+            <CreateHeader 
+                onSaveAsDraftButtonClick={() => handleHeaderSaveAsDraftButtonClick(code)}
+                onSubmitButtonClick={() => handleHeaderSubmitButtonClick(code)}
+            />
+            <div className="Page-content CreatePage-content">
+                <UpdateCode />
+                <UpdateGLSLCanvas {...canvasProps} />
+            </div>
+        </div>;
+    }
+}
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(CreatePage);
