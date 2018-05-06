c8d1e8d8c4c72dd238b1288f47d1ceb73a689344
r21nomi <ryotakosu@gmail.com>
2018-05-06 14:45:12 +0900

    Implement shader preview feature on art thumb

--- client/src/components/molecules/Art.tsx
+++ client/src/components/molecules/Art.tsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import ArtInfo from '../atoms/ArtInfo';
 import { ArtEntity } from '../../models';
 import './styles/art.css';
+import UpdateGLSLCanvas from '../../containers/UpdateGLSLCanvas';
 
 interface Props {
     art: ArtEntity;
@@ -10,17 +11,56 @@ interface Props {
     onToggleStar: (artId: String, isStarCurrent: boolean) => void;
 }
 
-const Art = ({ art, isMyPage, onToggleStar }: Props) => (
-    <div className="Art">
-        <Link className="Art-thumb" to={`/art/${art.id}`}>
-            <img src={art.thumb} alt={art.title}/>
-        </Link>
-        <ArtInfo
-            art={art}
-            isMyPage={isMyPage}
-            onToggleStar={onToggleStar}
-        />
-    </div>
-);
+class Art extends React.Component<Props, object> {
+    isMouseOver: boolean;
+
+    componentDidMount() {
+        this.isMouseOver = false;
+    }
+
+    render() {
+        const { art, isMyPage, onToggleStar } = this.props;
+
+        return <div className="Art">
+            <Link className="Art-thumb"
+                  to={`/art/${art.id}`}
+                  onMouseOver={() => {
+                      this.isMouseOver = true;
+                      this.forceUpdate();
+                  }}
+                  onMouseLeave={() => {
+                      this.isMouseOver = false;
+                      this.forceUpdate();
+                  }}>
+                <img ref="artThumb" src={art.thumb} alt={art.title}/>
+                {(() => {
+                    if (this.isMouseOver) {
+                        let artThumb: any = this.refs.artThumb;
+                        return <div className="Art-preview">
+                            <UpdateGLSLCanvas
+                                width={artThumb.width}
+                                height={artThumb.height}
+                                onCanvasUpdated={(gl: any) => {
+                                    // no-op
+                                }}
+                                vertexShader={art.codes[0].text}
+                                fragmentShader={art.codes[1].text}
+                                shouldRender={true}
+                                hasError={false}
+                            />
+                        </div>;
+                    } else {
+                        return null;
+                    }
+                })()}
+            </Link>
+            <ArtInfo
+                art={art}
+                isMyPage={isMyPage}
+                onToggleStar={onToggleStar}
+            />
+        </div>;
+    }
+}
 
 export default Art;
