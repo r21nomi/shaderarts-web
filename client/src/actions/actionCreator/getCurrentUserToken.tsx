fb735bf253ae1edfa5e845ada0f4cc3ff99fc704
r21nomi <ryotakosu@gmail.com>
2018-05-09 14:18:48 -0700

    Show content for non-login user

--- client/src/actions/actionCreator/getCurrentUserToken.tsx
+++ client/src/actions/actionCreator/getCurrentUserToken.tsx
@@ -4,7 +4,8 @@ export function getCurrentUserToken(): Promise<string> {
     let user: firebase.User | null = firebase.auth().currentUser;
 
     if (!user) {
-        return Promise.reject(new Error('Firebase user is not found.'));
+        console.log('Firebase user is not found.');
+        return Promise.resolve('');
     }
 
     return user!!.getIdToken(true).then(idToken => idToken);
