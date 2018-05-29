f3959ca7bc9a42bf7242b8af5532f42b11ea0315
r21nomi <ryotakosu@gmail.com>
2018-05-30 00:34:52 +0900

    Move to top page after logout

--- client/src/actions/actionCreator/logout.tsx
+++ client/src/actions/actionCreator/logout.tsx
@@ -1,11 +1,13 @@
 import * as firebase from 'firebase';
 import { LogoutAction } from '../logoutAction';
+import { push, RouterAction } from 'react-router-redux';
 
 export function logout() {
     return (dispatch: any) => {
         firebase.auth().signOut().then(function() {
             // firebase.auth().onAuthStateChanged in fetchUser.tsx will be fired.
             dispatch(logoutSuccess());
+            dispatch(goToTopPage());
 
         }).catch(error => dispatch(logoutError(error.message)));
     };
@@ -23,4 +25,8 @@ function logoutError(errorMessage: string): LogoutAction {
         type: 'LOGOUT_ERROR',
         errorMessage: errorMessage
     };
+}
+
+function goToTopPage(): RouterAction {
+    return push('/');
 }
